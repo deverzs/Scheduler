aa2fa9be0283ec8a0f8b5b8aed18fc58504de515
qtrong1011 <58069448+qtrong1011@users.noreply.github.com>
2020-04-07 01:21:57 -0700

    Almost done with Priority+RR

---
+++ ReadyQueue_priority_rr.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "PCB.h"
+/*
+Jason Luu and Zsuzsanna Dianovics
+Assignment 1
+Header file: ReadyQueue.h
+Header file for ReadyQueue object
+This object creats a priority queue that removes objects
+based on the priority of the PCB object
+*/
+
+/**
+ * ReadyQueue is a queue of PCB's that are in the READY state to be scheduled to run.
+ * It should be a priority queue here uch that the process with the highest priority
+ * can be selected next.
+ */
+class ReadyQueue {
+public:
+
+	// add a PCB representing a process into the ready queue.
+	void add(PCB* pcbPtr);
+
+	// remove and return the PCB with the highest priority from the queue
+	PCB* removeHighest();
+
+	// Returns the number of elements in the queue.
+	int size();
+
+	// Prints the queue contents to standard output based on the order of FCFS.
+	void display();
+	
+	//constructor
+	ReadyQueue();
+	
+	//debugging function to check how often the queue is empty
+	int howOftenEmpty();
+
+	//Check the front element in the queue AFTER reheapifying
+
+	PCB* checkFront();
+	
+	
+private:
+
+	//Utility functions for priority queue
+	void swap(int,int);		//swap the objects
+	void reheapify(); 		//re-arrange after remove from the ReadyQueue
+	int getLargerchild(int);//return the location of the smaller child
+	void trickleup(); 		// trickle up after adding at the rear
+	int getParent(int); 	//return the parent location given the child location
+	bool even(int); 		//even or odd?
+
+	PCB* Q[30]; 			//an array holding PCB elements.
+	int count; 				//how many elements are in the ReadyQueue
+	int emptyQ;				//number of times queue is empty
+	
+};
---
+++ schedule_priority_rr.cpp
@@ -0,0 +1,237 @@
+/**
+ * Driver program 
+ * 
+ * Add other data structures and .cpp and .h files as needed.
+ * 
+ * The input file is in the format:
+ *
+ *  [name], [priority], [CPU burst]
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "PCB.h"
+#include "ReadyQueue_priority_rr.h"
+#include "ReadyQueue_rr.h"
+
+
+using namespace std;
+
+void rrQueue(PCB* running_task, queue tempQueue, double sfinish_time, double swaiting_time, int QUANTUM, int arrive_time, int previous_finish_time){
+    while(!tempQueue.isEmpty()){
+                    running_task = tempQueue.remove();
+                    cout<<"Running task = ["<<running_task->name<<"]   ";
+                    cout<<"["<<running_task->priority<<"]   ";
+                    cout<<"["<<running_task->burst_left<<"]   for ";
+                    if(running_task->burst_left > QUANTUM){
+                        running_task->burst_left = running_task->burst_left - QUANTUM;
+                        //running_process->waiting_time = running_process->waiting_time + QUANTUM;
+                        tempQueue.add(running_task);
+                        previous_finish_time = previous_finish_time + QUANTUM;
+                        cout<<QUANTUM<<" units"<<endl;
+                        continue;
+                    }
+                    else{
+                        running_task->turn_around_time = running_task->turn_around_time + arrive_time + running_task->burst_left + previous_finish_time;
+                        previous_finish_time = running_task->turn_around_time;
+                        running_task->waiting_time = running_task->turn_around_time - running_task->burst;
+                        cout<<running_task->burst_left<<" units"<<endl;
+                        cout<<"Task "<<running_task->name<<" is finished."<<endl;
+                        sfinish_time = sfinish_time + running_task->turn_around_time;
+                        swaiting_time = swaiting_time + running_task->waiting_time;
+                    }
+                }
+}
+
+
+int main(int argc, char *argv[])
+{
+    std::cout << "CS 433 Programming assignment 3" << std::endl;
+    std::cout << "Author: xxxxxx and xxxxxxx" << std::endl;
+    std::cout << "Date: xx/xx/20xx" << std::endl;
+    std::cout << "Course: CS433 (Operating Systems)" << std::endl;
+    std::cout << "Description : **** " << std::endl;
+    std::cout << "=================================" << std::endl;
+    
+    int QUANTUM = 10;
+    // Check that input file is provided at command line
+    if(argc < 2 ) {
+        cerr << "Usage: " << argv[0] << " <input_file> [<time_quantum>]" << endl;
+        exit(1);
+    }
+
+    // Read the time quantum if provided.
+    if(argc >= 3) {
+        QUANTUM = atoi(argv[2]);
+    }
+
+    // Read task name, priority and burst length from the input file 
+    string name;
+    int priority;
+    int burst;
+
+    // open the input file
+    std::ifstream infile(argv[1]);
+    string line;
+    // create the table for PCB and ReadyQueue
+    PCBTable table;
+    // tableQueue holds the queue to run the processes
+    ReadyQueue tableQueue; 
+    queue tempQueue; //***
+    //printQueue holds ran processes for printing info
+    ReadyQueue printQueue; 
+    PCB* new_process = new PCB("",0,0,0,0,0); //holds the new process from the input file
+    //running_task holds the processes that has the highest priority
+    PCB* running_task = new PCB("",0,0,0,0,0);
+    //next_task holds the preocess that will be removed in the next iteration
+    PCB* next_task;
+    int counter = 0; //local variable for keeping track of how many processes do we have in the input file
+    int arrive_time = 0; //assumption of all the process arrive at time 0
+    int previous_finish_time = 0; //holds the previous finished time process.
+    // int finish_time = 0;
+    // int waiting_time = 0;
+    double sfinish_time = 0;
+    double swaiting_time = 0;
+    while(getline(infile, line) ) {
+        std::istringstream ss (line);
+        // Get the task name
+        getline(ss, name, ',');
+        
+        // Get the task priority 
+        string token;
+        getline(ss, token, ',');
+        priority = std::stoi(token);
+
+        // Get the task burst length 
+        getline(ss, token, ',');
+        burst = std::stoi(token);
+        
+        cout << name << " " << priority << " " << burst << endl;
+        // TODO: add the task to the scheduler's ready queue
+        // You will need a data structure, i.e. PCB, to represent a task
+        new_process = new PCB(name,priority,burst,burst,0,0); 
+        table.add(new_process);
+        tableQueue.add(new_process);
+        counter++;
+    }
+        
+
+    //TESTING FOR CORRECT PCBTable
+    for(int i = 0; i <table.getSize(); i++){
+        cout<<"["<<table.PCBTable[i]->name<<"]  ";
+        cout<<"["<<table.PCBTable[i]->priority<<"]  ";
+        cout<<"["<<table.PCBTable[i]->burst<<"]  "<<endl;
+    }
+    //TESTING FOR CORRECT ReadyQueue
+        tableQueue.display();
+
+    // running_task = q1.removeHighest();
+    // cout<<"Running task = ["<<running_task->name<<"]   ";
+    // cout<<"["<<running_task->priority<<"]   ";
+    // cout<<"["<<running_task->burst<<"]"<<endl;
+
+
+    // TODO: Add your code to run the scheduler and print out statistics
+    while(tableQueue.size() != 0){
+        running_task = tableQueue.removeHighest();
+        next_task = tableQueue.checkFront();
+        cout<<"Running task = ["<<running_task->name<<"]   ";
+        cout<<"["<<running_task->priority<<"]   ";
+        cout<<"["<<running_task->burst_left<<"]   for ";
+        
+        if(running_task->isEqual){
+            if(running_task->burst_left > QUANTUM && running_task->priority == next_task->priority){
+                running_task->burst_left = running_task->burst_left - QUANTUM;
+                //running_process->waiting_time = running_process->waiting_time + QUANTUM;
+                tempQueue.add(running_task);
+                previous_finish_time = previous_finish_time + QUANTUM;
+                cout<<QUANTUM<<" units@@"<<endl;
+                continue;
+            }
+            else if (running_task->burst_left > QUANTUM && ((running_task->priority != next_task->priority) || (tableQueue.size() == 0))){
+                running_task->burst_left = running_task->burst_left - QUANTUM;
+                //running_process->waiting_time = running_process->waiting_time + QUANTUM;
+                tempQueue.add(running_task);
+                previous_finish_time = previous_finish_time + QUANTUM;
+                cout<<QUANTUM<<" units"<<endl;
+                while(!tempQueue.isEmpty()){
+                    running_task = tempQueue.remove();
+                    cout<<"Running task = ["<<running_task->name<<"]   ";
+                    cout<<"["<<running_task->priority<<"]   ";
+                    cout<<"["<<running_task->burst_left<<"]   for ";
+                    if(running_task->burst_left > QUANTUM){
+                        running_task->burst_left = running_task->burst_left - QUANTUM;
+                        //running_process->waiting_time = running_process->waiting_time + QUANTUM;
+                        tempQueue.add(running_task);
+                        previous_finish_time = previous_finish_time + QUANTUM;
+                        cout<<QUANTUM<<" units"<<endl;
+                        continue;
+                    }
+                    else{
+                        running_task->turn_around_time = running_task->turn_around_time + arrive_time + running_task->burst_left + previous_finish_time;
+                        previous_finish_time = running_task->turn_around_time;
+                        running_task->waiting_time = running_task->turn_around_time - running_task->burst;
+                        cout<<running_task->burst_left<<" units"<<endl;
+                        cout<<"Task "<<running_task->name<<" is finished."<<endl;
+                        sfinish_time = sfinish_time + running_task->turn_around_time;
+                        swaiting_time = swaiting_time + running_task->waiting_time;
+                    }
+                }
+            }
+            else
+            {
+                running_task->turn_around_time = running_task->turn_around_time + arrive_time + running_task->burst_left + previous_finish_time;
+                previous_finish_time = running_task->turn_around_time;
+                running_task->waiting_time = running_task->turn_around_time - running_task->burst;
+                cout<<running_task->burst_left<<" units"<<endl;
+                cout<<"Task "<<running_task->name<<" is finished."<<endl;
+                sfinish_time = sfinish_time + running_task->turn_around_time;
+                swaiting_time = swaiting_time + running_task->waiting_time;
+            }
+        }
+        else
+        {
+            running_task->turn_around_time = running_task->turn_around_time + arrive_time + running_task->burst_left + previous_finish_time;
+            previous_finish_time = running_task->turn_around_time;
+            running_task->waiting_time = running_task->turn_around_time - running_task->burst;
+            cout<<running_task->burst_left<<" units"<<endl;
+            cout<<"Task "<<running_task->name<<" is finished."<<endl;
+            sfinish_time = sfinish_time + running_task->turn_around_time;
+            swaiting_time = swaiting_time + running_task->waiting_time;
+        }    
+    }
+    while(!tempQueue.isEmpty()){
+                    running_task = tempQueue.remove();
+                    cout<<"Running task = ["<<running_task->name<<"]   ";
+                    cout<<"["<<running_task->priority<<"]   ";
+                    cout<<"["<<running_task->burst_left<<"]   for ";
+                    if(running_task->burst_left > QUANTUM){
+                        running_task->burst_left = running_task->burst_left - QUANTUM;
+                        //running_process->waiting_time = running_process->waiting_time + QUANTUM;
+                        tempQueue.add(running_task);
+                        previous_finish_time = previous_finish_time + QUANTUM;
+                        cout<<QUANTUM<<" units"<<endl;
+                        continue;
+                    }
+                    else{
+                        running_task->turn_around_time = running_task->turn_around_time + arrive_time + running_task->burst_left + previous_finish_time;
+                        previous_finish_time = running_task->turn_around_time;
+                        running_task->waiting_time = running_task->turn_around_time - running_task->burst;
+                        cout<<running_task->burst_left<<" units"<<endl;
+                        cout<<"Task "<<running_task->name<<" is finished."<<endl;
+                        sfinish_time = sfinish_time + running_task->turn_around_time;
+                        swaiting_time = swaiting_time + running_task->waiting_time;
+                    }
+                }
+    table.display();
+    
+    cout<<"Average turn-around time = "<<sfinish_time/counter<<endl;
+    cout<<"Average waiting time = "<<swaiting_time/counter<<endl;
+    
+    return 0;
+}
