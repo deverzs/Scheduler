04954b8dd2d0ac1e175e4da99edbe19af69e19b8
qtrong1011 <58069448+qtrong1011@users.noreply.github.com>
2020-04-07 01:20:11 -0700

    ALMOST DONE WITH Priority+RR

--- PCB.cpp
+++ PCB.cpp
@@ -1,4 +1,8 @@
 #include "PCB.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
 /*
 Jason Luu and Zsuzsanna Dianovics
 Assignment 1
@@ -9,11 +13,30 @@ Implementation file: PCB.cpp
 	@params  id: int the id of the process
 			 priority: int the priority of the process
 */
-PCB::PCB(int id, int priority){
-  this->id = id;
+PCB::PCB(string name, int priority, int burst, int burst_left, int turn_around_time, int waiting_time){
+  this->name = name;
   this->priority = priority;
+  this->burst = burst;
+  this->burst_left = burst_left;
+  this->turn_around_time = turn_around_time;
+  this->waiting_time = waiting_time;
+  isEqual = false;
   state = ProcState::NEW;
 }
-
-
-
+// get the size of the PCB table and return its size
+int PCBTable::getSize(){
+  return count;
+}
+// to add the new process into the PCB table
+void PCBTable::add(PCB* process){
+  PCBTable[count] = process;
+  count++;
+}
+//to display the content of the PCB table
+void PCBTable::display(){
+  for(int i = 0; i <count;i++){
+      cout<<PCBTable[i]->name<<" turn-around time = ";
+      cout<<PCBTable[i]->turn_around_time<<", waiting time = ";
+      cout<<PCBTable[i]->waiting_time<<endl;
+  }
+}
--- PCB.h
+++ PCB.h
@@ -1,10 +1,11 @@
 #pragma once
+#include <string>
+using namespace std;
 
 /*
 Jason Luu and Zsuzsanna Dianovics
 Assignment 1
 PCB.h
-
 Header file: PCB object
 This object has an enum class representing the process state
 This object needs an ID and a Priority to be created
@@ -26,22 +27,39 @@ public:
 	// The current state of the process.
 	// A process in the ReadyQueue should be in READY state
 	ProcState state;
+	//The name of the process
+	string name;
+	//The length of burst
+	int burst;
+	//The length of burst left for RR algorithm
+	int burst_left;
+	//The turn-around time of the process
+	int turn_around_time;
+	//The waiting time of the process
+	int waiting_time;
 	//Tracking how often object removed from queue
 	int removed;
 	//Tracking how often added to the queue
 	int added;
+	//to check if they are the same priority
+	bool isEqual;
 	//constructor
-	PCB(int,int);
+	PCB(string, int, int,int, int, int);
 	
 };
 
 /*
 Header and implementation for PCBTable
-
 PCBTable is an array of PCB object pointers 
 */
 class PCBTable {
+ private:
+	int count = 0; //how many processes do we have right now
  public:
-  PCB* PCBTable[30]; //an array for PCB table, holds 30 pointers 
+	PCB* PCBTable[30]; //an array for PCB table, holds 30 pointers 
+	int getSize(); //return the size of PCB table
+	void add(PCB*); //add the new process into PCB table
+	void display(); //display the contents of the PCB table 
+	
   
 };
